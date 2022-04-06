735b123fa4233a4ca288f4e3b9b42b6e715bc9f9
an4132 <anvo2016026@gmail.com>
2022-04-05 19:12:14 -0600

    finish homepage functionality

--- app/src/main/java/com/bigbraingang/diaryapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/bigbraingang/diaryapp/ui/home/HomeFragment.java
@@ -1,32 +1,99 @@
 package com.bigbraingang.diaryapp.ui.home;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.bigbraingang.diaryapp.R;
 import com.bigbraingang.diaryapp.databinding.FragmentHomeBinding;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class HomeFragment extends Fragment {
-
+    private Button submitButton;
+    private TextView entry;
+    private RadioButton radioButton1;
+    private RadioButton radioButton2;
+    private RadioButton radioButton3;
+    private RadioButton radioButton4;
+    private RadioButton radioButton5;
     private FragmentHomeBinding binding;
+    DateFormat date = new SimpleDateFormat("MMM dd yyyy, h:mm");
 
+    //    private Toast toast;
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
-
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        submitButton = root.findViewById(R.id.homeSubmitButton);
+        entry = root.findViewById(R.id.entryTextBox);
+        radioButton1 = root.findViewById(R.id.radioButton1);
+        radioButton2 = root.findViewById(R.id.radioButton2);
+        radioButton3 = root.findViewById(R.id.radioButton3);
+        radioButton4 = root.findViewById(R.id.radioButton4);
+        radioButton5 = root.findViewById(R.id.radioButton5);
+
+
+        submitButton.setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void onClick(View view){
+               submitButton.setBackgroundColor(Color.RED);  // test to see if button is responsive.
+               int rating = getChecked();                                          // Save ratings
+               String textEntry = entry.getText().toString();                      // Save textEntry
+               String dateFormat = date.format(Calendar.getInstance().getTime());  // Save Date
+
+               // save to SQL
+
+               // Clear entries
+               Toast.makeText(getActivity(), String.valueOf(rating), Toast.LENGTH_LONG).show();
+
+               entry.setText("");
+               radioButton1.setChecked(false);
+               radioButton2.setChecked(false);
+               radioButton3.setChecked(false);
+               radioButton4.setChecked(false);
+               radioButton5.setChecked(false);
+           }
+        });
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
+    public int getChecked(){
+        if (radioButton1.isChecked()){
+            return 1;
+        }
+        if(radioButton2.isChecked()){
+            return 2;
+        }
+        if(radioButton3.isChecked()){
+            return 3;
+        }
+        if(radioButton4.isChecked()){
+            return 4;
+        }
+        if(radioButton5.isChecked()){
+            return 5;
+        }
+        return 3;
+    }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
